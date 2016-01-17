315b612e4487d258f532b64d7f01bca773464c29
Grazy51171 <grazy51171@gmail.com>
2016-01-17 18:16:36 +0100

    Quelques amélioration des notif

--- notifandthanks.js
+++ notifandthanks.js
@@ -1,12 +1,14 @@
-/* Title: "Rotating Notifier" bot
-    Author: badbadbubba + me
+/* Title: "Rotating Notifier" bot modified to ass thanks
+    Author : grazy
+    
+    Base on : 
+    Author: badbadbubba
     Version: 1.0.3 (04/11/2015)
 
     A simple rotating notifier bot.  Only options are to set color and display interval.  Messages are rotated in
     sequence.  Message 1 will also be displayed on room entry privately.
-    V1.0.1 - Removed unnecessary check for valid first messages
-    V1.0.2 - Increase messages to 5
-    V1.0.3 - Added option to display msg1 on room entry      
+    Thanks on tip (two level of message possible).
+    
 */
 
 var i=1;
@@ -22,86 +24,67 @@ cb.settings_choices = [
     {name:'msgback', type:'str', label:'Notice back color (html code default white #9F000F)', defaultValue: '#FFFFFF'},
     {name: 'chat_ad', type:'int', minValue: 1, maxValue: 999, defaultValue: 2,
         label: 'Delay in minutes between notices being displayed (minimum 1)'},
-{
-    name: 'tipamount',
-    type: 'int',
-    minValue: 1,
-    defaultValue: 1,
-    label: 'Minimum tokens required for a thank you message'
-}
-,
-{
-    name: 'tipmsg',
-    type: 'str',
-    minLength: 8,
-    maxLength: 128,
-    defaultValue: '****  Thank you [tipper] for your tip!!! (: **** ',
-    label: "Tip thank you message, [tipper] = tipper's name"
-},
+    {name: 'tipamount', type: 'int', minValue: 1, defaultValue: 1, 
+        label: 'Minimum tokens required for a thank you message'},
+    {name: 'tipmsg', type: 'str', minLength: 8, maxLength: 128,  
+        defaultValue: '****  Thank you [tipper] for your tip!!! (: **** ',
+        label: "Tip thank you message, [tipper] = tipper's name" },
+    {name: 'tipamount1', type: 'int', minValue: 10, defaultValue: 10, 
+        label: 'Minimum tokens required for a thank you message 2'},
+    {name: 'tipmsg1', type: 'str', minLength: 8, maxLength: 128,
+        defaultValue: '****  Thank you [tipper] for your tip!!!   !!!! **** ',
+        label: "Tip thank you message 2 , [tipper] = tipper's name"}
+        ];
 
-{
-    name: 'tipamount1',
-    type: 'int',
-    minValue: 10,
-    defaultValue: 10,
-    label: 'Minimum tokens required for a thank you message 2'
+function sendMessage(message, user) {
+    cb.sendNotice(message, user, cb.settings['msgback'], cb.settings['msgcolor'], 'bold');
 }
-,
-{
-    name: 'tipmsg1',
-    type: 'str',
-    minLength: 8,
-    maxLength: 128,
-    defaultValue: '****  Thank you [tipper] for your tip!!!   !!!! **** ',
-    label: "Tip thank you message 2 , [tipper] = tipper's name"
-}
-];
 
-cb.onEnter(function(user) {
+cb.onEnter(function (user) {
     if (cb.settings['msgonentry'] == 'yes') {
-        cb.sendNotice('Bienvenue ' + user['user'] + '! ' + cb.settings['msg1'],user['user'],cb.settings['msgback'],cb.settings['msgcolor'],'bold');
+        sendMessage('Bienvenue ' + user['user'] + '! \n ' + cb.settings['msg1'], user['user'])
+    }
+
+    if (user['user'] == 'grazy') {
+        // just for fun 
+        sendMessage('Bienvenue Grazy. :robot1 Ton bot te salue, maitre.','')
     }
 });
 
 function chatAd() {
     var msg;
-    
-        while (cb.settings['msg' + i] == 0) {      //skip empty messages
+
+    while (cb.settings['msg' + i] == 0) {      //skip empty messages
         i++;
-            if (i > 5) {                           //loop back to first message   
-                i=1;
-            }
-        }
-        
-        msg = cb.settings['msg' + i];
-        i++;
-        
         if (i > 5) {                           //loop back to first message   
-           i=1;
+            i = 1;
         }
-           
-        cb.sendNotice(msg,'',cb.settings['msgback'],cb.settings['msgcolor'],'bold');
-        cb.setTimeout(chatAd, (cb.settings.chat_ad * 60000));
+    }
+
+    msg = cb.settings['msg' + i];
+    i++;
+
+    if (i > 5) {                           //loop back to first message   
+        i = 1;
+    }
+
+    sendMessage(msg, '');
+    cb.setTimeout(chatAd, (cb.settings.chat_ad * 60000));
 }
+
 cb.setTimeout(chatAd, (cb.settings.chat_ad * 60000));
 
-cb.onTip(function (tip)
-{
-    if (parseInt(tip['amount']) >= cb.settings.tipamount1)
-    {
-        tipper = tip['from_user'];
-        tipmsg=cb.settings.tipmsg1;
+cb.onTip(function (tip) {
+    var tipper = tip['from_user'];
+    var tipmsg;
+    if (parseInt(tip['amount']) >= cb.settings.tipamount1) {
+        tipmsg = cb.settings.tipmsg1;
         tipmsg = tipmsg.replace("[tipper]", tipper);
-    cb.sendNotice(tipmsg,'',cb.settings['msgback'],cb.settings['msgcolor'],'bold');
-
-    } else
-    if (parseInt(tip['amount']) >= cb.settings.tipamount)
-    {
-        tipper = tip['from_user'];
-        tipmsg=cb.settings.tipmsg;
+        sendMessage(tipmsg, '');
+    } else if (parseInt(tip['amount']) >= cb.settings.tipamount) {
+        tipmsg = cb.settings.tipmsg;
         tipmsg = tipmsg.replace("[tipper]", tipper);
-    cb.sendNotice(tipmsg,'',cb.settings['msgback'],cb.settings['msgcolor'],'bold');
-
+        sendMessage(tipmsg, '');
     }
 });
 
